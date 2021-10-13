d56366e6d6b5cf81f3020cdc1b32fa3eb6e4a04d
RafaelDRicci <rafael_d_ricci@hotmail.com>
2021-10-13 10:34:47 -0300

    Implementando últimos testes e as funcinalidades das classes dos exercícios de JUnit

--- src/com/rafaeld/introducaojunit/MathUtil.java
+++ src/com/rafaeld/introducaojunit/MathUtil.java
@@ -5,6 +5,8 @@
  */
 package com.rafaeld.introducaojunit;
 
+import java.util.Objects;
+
 /**
  *
  * @author rafaeld
@@ -13,13 +15,38 @@ public class MathUtil {
  
     public static int mdc(int a, int b){
         
+        a = Math.abs(a);
+        b = Math.abs(b);
+        
+        
+        int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+        
+        
         if(b > 0 && a % b == 0) return b;
         
         if(b == 0) return Math.abs(a);
         
-        if(a % b != 0) return 1;
+        return mdc(a - b, b);
         
-        return -1;
+    }
+    
+    public static int mdc(int ...valores){
+        
+        Objects.requireNonNull(valores,
+                "O parâmetro valores não pode ser nulo para calcular o MDC");
+        
+        if(valores.length == 0){
+            throw new IllegalArgumentException(""
+                    + "É necessário informar ao menos um valore para calcular o MDC"); 
+        }
+        
+        int a = valores[0];
+        for (int b : valores) {
+            a = mdc (a, b);
+        }
+        return a;
     }
     
 }
--- test/com/rafaeld/introducaojunit/MathUtilTest.java
+++ test/com/rafaeld/introducaojunit/MathUtilTest.java
@@ -103,4 +103,72 @@ public class MathUtilTest {
         assertEquals(esperado, obtido);
     }
     
+    @Test
+    public void testMdcP6(){
+        final int a = 8;
+        final int b = 2;
+        //mdc(a,b) == mdc(b,c)
+        final int esperado = mdc(a,b);
+        final int obtido = mdc(b,a);
+        assertEquals(esperado, obtido);
+        
+    }
+    
+    
+    @Test
+    public void testMdcP7TudoNegativo(){
+        final int a = - 12;
+        final int b = -6;
+        final int esperado = 6;
+        final int obtido = mdc (a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    public void testMdcP7ANegativo(){
+        final int a = - 12;
+        final int b = - 6;
+        final int esperado = 6;
+        final int obtido = mdc (a,b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    public void testMdcP7BNegativo(){
+        final int a = 12;
+        final int b = - 6;
+        final int esperado = 6;
+        final int obtido = mdc (a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    public void testMdc30e12(){
+        final int a = 30;
+        final int b = 12;
+        final int esperado = 6;
+        final int obtido = mdc (a, b);
+        assertEquals (esperado, obtido);
+    }
+    
+    @Test 
+    public void testMdcMultiplosValores(){
+        final int a = 30;
+        final int b = 12;
+        final int c = 4;
+        final int esperado = 2;
+        final int obtido = mdc(a, b, c);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testMdcNenhumParamentro(){
+       mdc();
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testMdcValoresNull(){
+        mdc(null);
+    }
+    
 }
